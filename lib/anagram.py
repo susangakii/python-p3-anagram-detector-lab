c75d862cc7fc9e09374d1b3b4cc68a84d1f1a4bf
Susan Gakii <susangakii71@gmail.com>
2025-11-20 08:50:36 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        matches = []
+        sorted_original = sorted(self.word.lower())
+
+        for w in word_list:
+            if sorted(w.lower()) == sorted_original:
+                matches.append(w)
+
+        return matches
